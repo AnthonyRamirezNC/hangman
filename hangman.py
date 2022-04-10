8dde9f3e126f61f20334ed0b726ddbbde8b368f1
AnthonyRamirezNC <thearamirez2020@gmail.com>
2022-04-10 17:23:13 -0400

    Need to delete previous text label and update img

--- hangman.py
+++ hangman.py
@@ -24,7 +24,7 @@ Window.columnconfigure(1, weight=2)
 frame = Frame(Window, width=600, height=400)
 frame.grid(column=1,row=0)
 
-
+updatelabel = ""
 
 #hangman pictures
 hangmanbase = ImageTk.PhotoImage(Image.open("hangmanbase.jpg"))
@@ -43,47 +43,66 @@ clsbtn = Button(Window, text = "Click To Close Game",command = Window.destroy).g
 
 
 
-def submit(my_text_box,rndmword,word):
-    submitch = Button(Window, text = "Submit",command =lambda: getInput(my_text_box,rndmword,word))
+def submit(my_text_box,rndmword,word,wordlabel):
+    submitch = Button(Window, text = "Submit",command =lambda: getInput(my_text_box,rndmword,word,wordlabel))
     submitch.grid(column=0,row=0,sticky='N',pady=50)
 
 def startgame():
-    textbox(generateword(0),generateword(1))
-    label = Label(frame,image = hangmanbase).pack()
-
-def generateword(flow):
     word = []
     rndmword = random.choice(words)
+    print(rndmword)
+    randmword = generateword(0,word,rndmword)
+    label = Label(frame,image = hangmanbase).pack()
+    
+    
+
+def generateword(flow,word,rndmword):
+    global wordlabel
     for chrcnt in rndmword:
-        word.append("_")
-    wordlabel = Label(Window, text = word, font = ("Ariel",25)).grid(column=0,row=0,pady=150,sticky = 'N')
+        word.append("X")
+    wordlabel = Label(Window, text = word, font = ("Ariel",20)).grid(column=0,row=0,pady=150,sticky = 'N')
+    
+    textbox(rndmword,word,wordlabel)#need to change get word function to update label
+    return(rndmword)
+
+
+def getword(rndmword,flow):#need to change function to update label
     if flow == 0:
-        return(rndmword)
-    elif flow == 1:
-        return(word)
+        return rndmword
+    elif flow ==1:
+        return 
 
 
 
-def getInput(my_text_box,rndmword,word):
+def getInput(my_text_box,rndmword,word,wordlabel):
     inpt = my_text_box.get("1.0","end-1c")
     #what to do with inpt after submitting, can add functions here
-    print(rndmword)
-    print(word)
-    #checkanswer(inpt,rndmword,word)
+    checkanswer(inpt,rndmword,word,wordlabel,updatelabel)
 
-def textbox(rndmword,word):    
+def textbox(rndmword,word,wordlabel):    
     my_text_box = Text(Window,height=5, width = 10)
     my_text_box.grid(column=0,row=0,sticky='N',pady=75)
-    submit(my_text_box,rndmword,word)
+    submit(my_text_box,rndmword,word,wordlabel)
     return my_text_box
 
-def checkanswer(inpt,rndmword,word):
-   for index, char in rndmword:
-       if char == inpt:
-           word[index].replace(inpt)
+def checkanswer(inpt,rndmword,word,wordlabel,updatelabel): ######################################
+    for index, char in enumerate(rndmword):
+        if char == inpt:
+            updatelabel = updatelabel + inpt
+        elif char != inpt:
+            updatelabel = updatelabel + "X"
+        updt = getupdtlbl(updatelabel,0)
     
-        
-           
+    Label(Window, text = updt, font = ("Ariel",20)).grid(column=0,row=0,pady=150,sticky = 'N')
+
+def getupdtlbl(updatelbl,flow):
+    if flow == 0:
+        return(updatelbl)
+    #elif flow ==1:
+
+
+
+
 
 def setimage(count,label):
     if count == 1:
